8960a4950c1436a74c84f2c93cdf502eb4588ac3
wilsonlargo <136187461+wilsonlargo@users.noreply.github.com>
2024-07-03 10:19:52 -0500

    update gis

--- app-web/app-GIS/config/map-config.js
+++ app-web/app-GIS/config/map-config.js
@@ -1,7 +1,13 @@
-
+const rango = {
+    "Muy Alta": ["1"],
+    "Alta": ["0.8"],
+    "Moderada": ["0.6"],
+    "Baja": ["0.4"],
+    "Muy Baja": ["0.2"],
+    
+}
 function openfile(control) {
     const archivo = control.target.files[0];
-
     if (!archivo) {
         return;
     }
@@ -9,17 +15,28 @@ function openfile(control) {
     lector.onload = function (e) {
         var contenido = e.target.result;
         var Parse = JSON.parse(contenido)
-
         L.geoJSON(Parse, {
             style: function (feature) {
-                //return {color: feature.properties.color};
+                //console.log(feature.properties.CLASIFICAC)
+                const propiedad_color = feature.properties.CLASIFICAC
+                //console.log(rango["Alto"][0])
+                try {
+                    return {
+                        color:"red",
+                        fillColor: "red",
+                        fillOpacity:rango[propiedad_color][0],
+                        weight: 1,
+                    };
+                } catch (error) {
+                    return { color: "",fillOpacity:0 };
+                }
             }
         }).bindPopup(function (layer) {
-            //return layer.feature.properties.description;
+            return layer.feature.properties.CLASIFICAC;
         }).addTo(map);
 
-        //console.log(contenido)
+
     };
     lector.readAsText(archivo);
-   
+
 }
